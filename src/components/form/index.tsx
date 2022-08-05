90eea919230e1a7f3d084fddf3e0edd4dfaf5249
MasterKelwin <mkelwin@outlook.com>
2022-08-05 09:12:38 -0300

    checkpoint

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,30 +1,36 @@
-import React from "react";
-import Button from "../button"
+import React from 'react';
+import Button from '../button';
+import style from './form.module.scss';
 
 class Form extends React.Component {
-    render() {
-        return(
-            <form>
-                <div>
-                    <label htmlFor="task"></label>
-                    <input type="text" id="task" required />
-                </div>
-                <div>
-                    <label htmlFor="time"></label>
-                    <input 
-                        type="time"
-                        id="time"
-                        min={"00:00:01"}
-                        max={"02:30:00"}
-                        step="1"
-                        name="time"
-                        required
-                    />
-                </div>
-                <Button />
-            </form>
-        )
-    }
+  render() {
+    return(
+      <form className={style.form}>
+        <div className={style.divTask}>
+          <label htmlFor="task">Insira sua tarefa</label>
+          <input 
+            className={style.input}
+            type="text" 
+            id="task" 
+            required />
+        </div>
+        <div className={style.divTime}>
+          <label htmlFor="time">Insira o tempo desejado</label>
+          <input 
+              className={style.input}
+              type="time"
+              id="time"
+              min={"00:00:01"}
+              max={"02:30:00"}
+              step="1"
+              name="time"
+              required
+          />
+        </div>
+        <Button />
+      </form>
+    )
+  }
 }
 
 export default Form;
